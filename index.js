3a58eebd8b1180f1055317e3cd6eb0225425561f
Tim Bijnen <>
2019-04-03 21:43:03 +0200

    Add start stop commands

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+
+
+const GPIO = require('onoff').Gpio
+const VALVE = new GPIO(27, 'out');
+
+app.get('/start', (req, res) => {
+    VALUE.writeSync(1);
+    res.send("Started watering")
+})
+app.get("/stop", (req, res) => {
+    VALUE.writeSync(0);
+    res.send("Stopped watering");
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
