4aa4810b3972995724e259ff4dba4d3889375509
Tim Bijnen <>
2020-10-26 10:25:32 +0100

    Cleanup

--- src/controllers/rpiController.js
+++ src/controllers/rpiController.js
@@ -1,23 +1,29 @@
 class RpiController {
-    static setStatus(req, res) {
-        return res.status(200).json({
-            data: { pins: [] },
-            message: "All the students",
-        });
-    }
-    // Get a single student
     static getStatus(req, res) {
         return res.status(200).json({
             data: { pins: [] },
             message: "All the pins",
         });
     }
+    static setStatus(req, res) {
+        return res.status(200).json({
+            data: { pins: [] },
+            message: "set status of multiple pins",
+        });
+    }
+
 
     static getSingleStatus(req, res) {
         const id = req.params.id;
         return res.status(200).json({
             data: { pins: [] },
             message: `Pin with id ${ id }`,
+        });
+    }
+    static setSingleStatus(req, res) {
+        return res.status(200).json({
+            data: { pins: [] },
+            message: "set status of multiple pins",
         });
     }
 }
--- src/routes/rpi.js
+++ src/routes/rpi.js
@@ -2,6 +2,8 @@ const RpiController = require( '../controllers/rpiController' );
 const routes = require("express").Router();
 
 routes.get('/rpi', RpiController.getStatus);
+routes.post('/rpi', RpiController.setStatus);
 routes.get('/rpi/:id', RpiController.getSingleStatus);
+routes.post('/rpi/:id', RpiController.setSingleStatus);
 
 module.exports = routes;
