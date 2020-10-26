252bfd2b109d634c3355fa4c77d1780f85fdb7a8
Tim Bijnen <>
2020-10-26 10:35:03 +0100

    Add test

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
+.PHONY: test
+
 run: 
 	npm run dev
 
 start:
 	npm start
+
+test:
+	npm run test
 
 attach:
 	tmux a -t api
---
+++ test/routes.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../src/app');
+// Configure chai
+chai.use(chaiHttp);
+chai.should();
+describe("rpi", () => {
+    describe("GET /rpi", () => {
+        it("should get rpi status", (done) => {
+             chai.request(app)
+                 .get('/rpi')
+                 .end((err, res) => {
+                     res.should.have.status(200);
+                     res.body.should.be.a('object');
+                     done();
+                  });
+        });
+
+        it("should get rpi status of a single pin", (done) => {
+            chai.request(app)
+                .get('/rpi/27')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                 });
+        });
+    });
+});
