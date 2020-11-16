9116696beb29a6f653815ed02d1fd34a29d99648
Tim Bijnen <>
2020-11-16 16:36:04 +0100

    Remove faulty pin config

--- config/pins.js
+++ config/pins.js
@@ -3,8 +3,8 @@ const PinType = require("../src/enums/PinType");
 const pins = [
     { name: "gardenLightsBack", number: 27, type: PinType.OUT, inverted: true },
     { name: "gardenLightsFront", number: 17, type: PinType.OUT, inverted: true },
-    { name: "gardenLightsBorderBack", number: 37, type: PinType.OUT, inverted: true },
-    { name: "gardenLightsBorderRight", number: 47, type: PinType.OUT, inverted: true },
+    // { name: "gardenLightsBorderBack", number: 37, type: PinType.OUT, inverted: true },
+    // { name: "gardenLightsBorderRight", number: 47, type: PinType.OUT, inverted: true },
 ];
 
 module.exports = pins;
